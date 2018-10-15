734b4e84262dff90df16cfe6c119f31d920ef321
蔡忠林 <790827284@qq.com>
2018-10-16 01:11:29 +0800

    commit change and delete demo

--- Assets/Src/Data/BaseGameInfo.cs
+++ Assets/Src/Data/BaseGameInfo.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BaseGameInfo : MonoBehaviour {
 
+    public Dictionary<int, card> cardInfo;
+    public Dictionary<int, equip> weaponInfo;
+    public Dictionary<int, fellow> fellowInfo;
+
     private static BaseGameInfo m_Instance;
     public static BaseGameInfo Instance
     {
@@ -12,6 +16,48 @@ public class BaseGameInfo : MonoBehaviour {
             if (m_Instance == null)
                 m_Instance = new BaseGameInfo();
             return m_Instance;
+        }
+    }
+
+    public class card
+    {
+        public string name;
+        public int skillType;
+        public int damageType;
+        public int damageScale;
+        public int cardValue;
+        public int cardId;
+    }
+    public class equip
+    {
+        public string name;
+        public int physicalAttack;
+        public int magicAttack;
+        public List<int> initCard;
+        public int equipValue;
+        public int CanEquipNum;
+    }
+    public class fellow
+    {
+        public string name;
+        public int initPhysicalAttack;
+        public int initMagicAttack;
+        public float initAttackSpeed;
+        public int initPhysicalDefense;
+        public int initMagicDefense;
+        public int initHealth;
+        public int initWeapon;
+        public int fellowQuality;
+
+        public class fightData
+        {
+            public int PhysicalAttack;
+            public int MagicAttack;
+            public float AttackSpeed;
+            public int PhysicalDefense;
+            public int MagicDefense;
+            public int Health;
+            public int Weapon;
         }
     }
     public int baseEnemyBlood = 100;
@@ -37,8 +83,87 @@ public class BaseGameInfo : MonoBehaviour {
         EnemyLv = EnemyLv + 1;
     }
 
-	// Update is called once per frame
-	void Update () {
+    public void saveInfo()
+    {
+        cardInfo = new Dictionary<int, card>();
+        weaponInfo = new Dictionary<int, equip>();
+        fellowInfo = new Dictionary<int, fellow>();
+        TextAsset info = Resources.Load<TextAsset>("config/cardInfo");
+        JSONObject json = new JSONObject(info.text);
+        JSONObject cardJson = new JSONObject(info.text).GetField("Card");
+        JSONObject weaponJson = new JSONObject(info.text).GetField("Weapon");
+        JSONObject cardListJson = new JSONObject(info.text).GetField("CardList");
+        List<JSONObject> cardList = cardListJson.list;
+        for(int i = 0; i < cardList.Count; i++)
+        {
+            card subCard = new card();
+            int cardId = (int)cardList[i].n;
+            JSONObject cardData = cardJson.GetField(cardId + "");
+            subCard.name = cardData.GetField("name").str;
+            subCard.skillType = (int)cardData.GetField("skillType").n;
+            subCard.damageType = (int)cardData.GetField("damageType").n;
+            subCard.damageScale = (int)cardData.GetField("damageScale").n;
+            subCard.cardValue = (int)cardData.GetField("cardValue").n;
+            //Debug.LogError(subCard.name + " " + subCard.skillType + " " + subCard.damageType + " " + subCard.damageScale + " " + subCard.cardValue);
+            cardInfo.Add(cardId, subCard);
+        }
+        for(int i = 0; i < weaponJson.Count; i++)
+        {
+            equip subWeapon = new equip();
+            int index = i + 1;
+            JSONObject weaponData = weaponJson.GetField(index + "");
+            subWeapon.name = weaponData.GetField("name").str;
+            subWeapon.physicalAttack = (int)weaponData.GetField("physicalAttack").n;
+            subWeapon.magicAttack = (int)weaponData.GetField("magicAttack").n;
+            List<JSONObject> card = weaponData.GetField("initCard").list;
+            subWeapon.initCard = new List<int>();
+            for (int j = 0; j < card.Count; j++)
+            {
+                subWeapon.initCard.Add((int)card[j].n);
+            }
+            subWeapon.equipValue = (int)weaponData.GetField("equipValue").n;
+            subWeapon.CanEquipNum = (int)weaponData.GetField("CanEquipNum").n;
+            //Debug.LogError(subWeapon.name+" "+ subWeapon.physicalAttack + " " + subWeapon.magicAttack + " " + subWeapon.initCard.Count + " " + subWeapon.equipValue + " " + subWeapon.CanEquipNum);
+            weaponInfo.Add(index, subWeapon);
+        }
+        info = Resources.Load<TextAsset>("config/fellowInfo");
+        JSONObject fellowJson = new JSONObject(info.text).GetField("Fellow");
+        for (int i = 0; i < fellowJson.Count; i++)
+        {
+            fellow subFellow = new fellow();
+            int index = i + 1;
+            JSONObject fellowData = fellowJson.GetField(index + "");
+            subFellow.name = fellowData.GetField("name").str;
+            subFellow.initPhysicalAttack = (int)fellowData.GetField("initPhysicalAttack").n;
+            subFellow.initMagicAttack = (int)fellowData.GetField("initMagicAttack").n;
+            subFellow.initAttackSpeed = fellowData.GetField("initAttackSpeed").f;
+            subFellow.initPhysicalDefense = (int)fellowData.GetField("initPhysicalDefense").n;
+            subFellow.initMagicDefense = (int)fellowData.GetField("initMagicDefense").n;
+            subFellow.initHealth = (int)fellowData.GetField("initHealth").n;
+            subFellow.initWeapon = (int)fellowData.GetField("initWeapon").n;
+            subFellow.fellowQuality = (int)fellowData.GetField("fellowQuality").n;
+            //Debug.LogError(subFellow.name + " " + subFellow.initPhysicalAttack + " " + subFellow.initMagicAttack + " " + subFellow.initPhysicalDefense + " " + subFellow.initMagicDefense + " " + subFellow.initHealth + " " + subFellow.initWeapon + " " + subFellow.fellowQuality);
+            fellowInfo.Add(index, subFellow);
+        }
+    }
+    public List<fellow.fightData> calculateAttribute(List<BaseGameInfo.fellow> fellowData)
+    {
+        List<fellow.fightData> fightData = new List<fellow.fightData>();
+        for (int i = 0; i < fellowData.Count; i++)
+        {
+            fellow.fightData subFellow = new fellow.fightData();
+            subFellow.PhysicalAttack = fellowData[i].initPhysicalAttack + weaponInfo[fellowData[i].initWeapon].physicalAttack;
+            subFellow.MagicAttack = fellowData[i].initMagicAttack + weaponInfo[fellowData[i].initWeapon].magicAttack;
+            subFellow.PhysicalDefense = fellowData[i].initPhysicalDefense;
+            subFellow.MagicDefense = fellowData[i].initMagicDefense;
+            subFellow.AttackSpeed = fellowData[i].initAttackSpeed;
+            subFellow.Health = fellowData[i].initHealth;
+            fightData.Add(subFellow);
+        }
+        return fightData;
+    }
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
--- Assets/Src/GameStart.cs
+++ Assets/Src/GameStart.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,16 +9,17 @@ public class GameStart : MonoBehaviour {
 
     public Transform canvas;
     public int InitEnemyHealth;
-    public int InitMyHealth;
+    //public int InitMyHealth;
     public int InitEnemyAttack;
     public int InitMyAttack;
     public float emenyAttackSpeed = 1.5f;
-    public float[] myAttackSpeed;
+    float[] myAttackSpeed;
     GameObject canvasPart;
     GameObject EnemyUIPart;
     GameObject FellowUIPart;
     int enemyHealth;
     int myHealth;
+    int myMaxHealth;
     private float mUpdateEnemySeconds = 0f;
     private float[] mUpdateMySeconds;
     Text EnemyName;
@@ -32,9 +34,11 @@ public class GameStart : MonoBehaviour {
     List<Text> MyHealth;
     Text MyHealthText;
     List<Text> MyTimeLeft;
-    List<Text> MyBattleInfo;
+    //List<Text> MyBattleInfo;
     BaseGameInfo baseGame;
-    
+    List<int> skillList;
+    List<BaseGameInfo.fellow> fellowData = new List<BaseGameInfo.fellow>();
+    List<BaseGameInfo.fellow.fightData> fellowFightData = new List<BaseGameInfo.fellow.fightData>();
     int fellowNum;
 
     // Use this for initialization
@@ -42,9 +46,23 @@ public class GameStart : MonoBehaviour {
         canvasPart = canvas.gameObject;
         EnemyUIPart = FindElementGo(canvasPart,"Game/EnemyUI");
         FellowUIPart = FindElementGo(canvasPart, "Game/FellowUI");
-        fellowNum = myAttackSpeed.Length;
-        mUpdateMySeconds = new float[fellowNum];
         baseGame = new BaseGameInfo();
+        baseGame.saveInfo();
+
+        fellowNum = 5;
+        myAttackSpeed = new float[fellowNum];
+        myHealth = 0;
+        for (int i = 0; i < fellowNum; i++)
+        {
+            int id = i + 1;
+            fellowData.Add(baseGame.fellowInfo[id]);
+        }
+        fellowFightData = baseGame.calculateAttribute(fellowData);
+        for(int i = 0; i < fellowNum; i++)
+        {
+            myAttackSpeed[i] = fellowFightData[i].AttackSpeed;
+        }
+        mUpdateMySeconds = new float[fellowNum];
         InitUIPart();
     }
 
@@ -77,7 +95,6 @@ public class GameStart : MonoBehaviour {
 	}
     void InitUIPart()
     {
-        
         GameObject Cover = FindElementGo(canvasPart, "Game/Cover");
         EnemyName = FindElementGo(EnemyUIPart, "GrpFrame/List/Name").GetComponent<Text>();
         EnemyHealth = FindElementGo(EnemyUIPart, "GrpFrame/List/Health").GetComponent<Text>();
@@ -85,25 +102,40 @@ public class GameStart : MonoBehaviour {
         EnemyHealthSlider = FindElementGo(EnemyUIPart, "EnemyHealth").GetComponent<Slider>();
         EnemyTimeLeft = FindElementGo(EnemyUIPart, "GrpFrame/List/TimeLeft").GetComponent<Text>();
         EnemySkillSlider = FindElementGo(EnemyUIPart, "EnemySkill").GetComponent<Slider>();
+        skillList = new List<int>();
         MyName = new List<Text>();
         MyHealth = new List<Text>();
         MyTimeLeft = new List<Text>();
-        MyBattleInfo = new List<Text>();
+        List<Text> MyWeaponInfo = new List<Text>();
+        //MyBattleInfo = new List<Text>();
         FellowSkillSlider = new List<Slider>();
         for (int i = 0; i < fellowNum; i++)
         {
             string name = "FellowUI" + (i + 1);
+            BaseGameInfo.fellow fellow = fellowData[i];
+
+
             GameObject FellowUI = FindElementGo(FellowUIPart, name);
             FellowUI.SetActive(true);
             MyName.Add(FindElementGo(FellowUIPart, name+"/List/Name").GetComponent<Text>());
             MyHealth.Add(FindElementGo(FellowUIPart, name + "/List/Health").GetComponent<Text>());
             MyTimeLeft.Add(FindElementGo(FellowUIPart, name + "/List/TimeLeft").GetComponent<Text>());
-            MyBattleInfo.Add(FindElementGo(FellowUIPart, name + "/List/BattleInfo").GetComponent<Text>());
+            MyWeaponInfo.Add(FindElementGo(FellowUIPart, name + "/List/WeaponInfo").GetComponent<Text>());
+            //MyBattleInfo.Add(FindElementGo(FellowUIPart, name + "/List/BattleInfo").GetComponent<Text>());
             FellowSkillSlider.Add(FindElementGo(FellowUIPart, "FellowSkill"+(i+1)).GetComponent<Slider>());
-            MyName[i].text = "刘备"+i;
-            MyHealth[i].text = myHealth.ToString();
-            MyTimeLeft[i].text = myAttackSpeed[0].ToString();
+            int weaponId = fellowData[i].initWeapon;
+            List<int> fellowSkillList = baseGame.weaponInfo[weaponId].initCard;
+            int randomSkillIndex = UnityEngine.Random.Range(0, fellowSkillList.Count);
+            skillList.Add(fellowSkillList[randomSkillIndex]);
+            MyName[i].text = fellow.name;
+            MyHealth[i].text = fellow.initHealth.ToString();
+            MyTimeLeft[i].text = myAttackSpeed[i].ToString();
+            MyWeaponInfo[i].text = baseGame.weaponInfo[weaponId].name;
+            FellowSkillSlider[i].gameObject.GetComponentInChildren<Text>().text = baseGame.cardInfo[skillList[i]].name;
+
+            myHealth += fellowFightData[i].Health;
         }
+        myMaxHealth = myHealth;
         if (fellowNum < 5)
         {
             for(int i = fellowNum; i < 5; i++)
@@ -118,15 +150,14 @@ public class GameStart : MonoBehaviour {
         MyHealthSlider = FindElementGo(FellowUIPart, "MyHealth").GetComponent<Slider>();
         
         enemyHealth = baseGame.EnemyLv * baseGame.EnemyLv * baseGame.baseEnemyBlood;
-        myHealth = InitMyHealth;
         EnemyName.text = "呂布";
         EnemyHealth.text = enemyHealth.ToString();
         EnemyHealthText.text = enemyHealth + "/" + baseGame.EnemyLv * baseGame.EnemyLv * baseGame.baseEnemyBlood;
         EnemyHealthSlider.value = enemyHealth / (float)(baseGame.EnemyLv * baseGame.EnemyLv * baseGame.baseEnemyBlood);
         EnemyTimeLeft.text = emenyAttackSpeed.ToString();
         
-        MyHealthText.text = myHealth + "/" + InitMyHealth;
-        MyHealthSlider.value = myHealth / (float)InitMyHealth;
+        MyHealthText.text = myHealth + "/" + myMaxHealth;
+        MyHealthSlider.value = myHealth / (float)myMaxHealth;
         Cover.GetComponent<Button>().onClick.AddListener(() =>
         {
             if(myHealth <= 0)
@@ -153,20 +184,40 @@ public class GameStart : MonoBehaviour {
             MyHealth[i].text = myHealth.ToString();
             MyTimeLeft[i].text = mUpdateMySeconds[i].ToString();
             FellowSkillSlider[i].value = (myAttackSpeed[i] - mUpdateMySeconds[i]) / myAttackSpeed[i];
-            FellowSkillSlider[i].gameObject.GetComponentInChildren<Text>().text = "普攻";
         }
-        MyHealthText.text = myHealth + "/" + InitMyHealth;
-        MyHealthSlider.value = myHealth / (float)InitMyHealth;
+        MyHealthText.text = myHealth + "/" + myMaxHealth;
+        MyHealthSlider.value = myHealth / (float)myMaxHealth;
     }
     void EnemyDoAttack()
     {
-        myHealth = myHealth - 10;
+        myHealth = myHealth - 10 - baseGame.EnemyLv * baseGame.EnemyLv;
     }
 
     void MeDoAttack(int i)
     {
-        enemyHealth = enemyHealth - 10;
-        MyBattleInfo[i].text = MyBattleInfo[i].text + "/n造成了10点伤害";
+        int doSkillId = skillList[i];
+        int skillType = baseGame.cardInfo[doSkillId].damageType;
+        int damage = 0;
+        if(skillType == 1)
+        {
+            damage = baseGame.cardInfo[doSkillId].damageScale * fellowFightData[i].PhysicalAttack;
+        }
+        else if(skillType == 2)
+        {
+            damage = baseGame.cardInfo[doSkillId].damageScale * fellowFightData[i].MagicAttack;
+        }
+        else if(skillType == 5)
+        {
+            damage = baseGame.cardInfo[doSkillId].damageScale * Math.Max(fellowFightData[i].PhysicalAttack, fellowFightData[i].MagicAttack);
+        }
+
+        enemyHealth = enemyHealth - damage;
+        int weaponId = fellowData[i].initWeapon;
+        List<int> fellowSkillList = baseGame.weaponInfo[weaponId].initCard;
+        int randomSkillIndex = UnityEngine.Random.Range(0, fellowSkillList.Count);
+        skillList[i] = fellowSkillList[randomSkillIndex];
+        FellowSkillSlider[i].gameObject.GetComponentInChildren<Text>().text = baseGame.cardInfo[skillList[i]].name;
+        //MyBattleInfo[i].text = MyBattleInfo[i].text + "/n造成了10点伤害";
     }
     void EndGame()
     {
